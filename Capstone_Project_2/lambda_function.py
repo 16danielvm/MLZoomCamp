10a1782380bf007cfb7b4617b7ae0e86f0ae35a5
Daniel Munoz <danni-0516@hotmail.com>
2023-12-25 13:15:53 -0600

    Final Model completed

---
+++ Capstone_Project_2/lambda_function.py
@@ -0,0 +1,32 @@
+import tensorflow.lite as tflite # se supone que se comenta este
+# import tflite_runtime.interpreter as tflite # PARA EL DOCKERFILE DESCOMENTAR ESTE!! Y COMENTAR EL DE ARRIBA
+from keras_image_helper import create_preprocessor
+
+interpreter = tflite.Interpreter(model_path='../final_model/mask-model.tflite')
+interpreter.allocate_tensors()
+
+preprocessor = create_preprocessor('xception', target_size=(224, 224))
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+classes =['WithMask', 'WithoutMask']
+
+# url = 'http://bit.ly/mlbookcamp-pants'
+
+def predict(url):
+    
+    X = preprocessor.from_url(url)
+    
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+
+    float_predictions = preds[0].tolist()
+    
+    return dict(zip(classes, float_predictions))
+
+def lambda_handler(event, context):
+    url = event['url']
+    result = predict(url)
+    return result

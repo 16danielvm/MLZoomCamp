e62cddf76b437508f734a98b477c5eab2beaf011
Daniel Munoz <danni-0516@hotmail.com>
2023-12-25 20:05:08 -0600

    Serverless

--- Capstone_Project_2/testServeless.py
+++ Capstone_Project_2/testServeless.py
@@ -1,8 +1,6 @@
 import requests
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-
-# url = 'https://zoiz6i4tu8.execute-api.us-east-2.amazonaws.com/test/predict'
+url = 'https://hvr312vdj8.execute-api.us-east-2.amazonaws.com/test_mask/predict'
 
 data = {'url': 'https://raw.githubusercontent.com/16danielvm/MLZoomCamp/master/Capstone_Project_2/final_model/45.png'}
 
